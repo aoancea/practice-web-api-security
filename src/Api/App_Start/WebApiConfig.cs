bad378024d2e85cf4c7ee68e4e6147b825484f63
Alex Oancea <alx.oancea@gmail.com>
2016-09-06 07:40:33 +0300

    feat(infrastructure): added JsonMediaTypeFormatter

--- src/Api/App_Start/WebApiConfig.cs
+++ src/Api/App_Start/WebApiConfig.cs
@@ -1,18 +1,26 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
 
 namespace Phobos.Api.App_Start
 {
-    public class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes();
+	public class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/v1/{controller}/{uid}",
-                defaults: new { uid = RouteParameter.Optional }
-            );
-        }
-    }
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/v1/{controller}/{uid}",
+				defaults: new { uid = RouteParameter.Optional }
+			);
+
+
+			config.Formatters.Clear();
+			config.Formatters.Add(new JsonMediaTypeFormatter());
+
+			config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+			config.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+		}
+	}
 }
