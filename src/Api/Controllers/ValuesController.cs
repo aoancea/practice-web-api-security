5dea3872891a0b8cde9abb5dd39de9b9cd165f69
Alex Oancea <alx.oancea@gmail.com>
2016-09-06 07:35:48 +0300

    feat(values): adding values controller

---
+++ src/Api/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Phobos.Api.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [ActionName("Get")]
+        public async Task<string[]> GetAsync()
+        {
+            return await Task.FromResult(new string[5] { "string1", "string2", "string3", "string4", "string5" });
+        }
+    }
+}
