9afa81597f1fa835101007e025c5d0b0462409d7
Alex Oancea <alx.oancea@gmail.com>
2016-09-10 21:38:51 +0300

    feat(security-token): added token generation endpoint

---
+++ src/Api/Infrastructure/SimpleOAuthAuthorizationServerProvider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using Phobos.Api.Context;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Phobos.Api.Infrastructure
+{
+    public class SimpleOAuthAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            using (ApplicationDbContext dbContext = new ApplicationDbContext())
+            {
+                var userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(dbContext));
+
+                IdentityUser user = await userManager.FindAsync(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            var ticket = new AuthenticationTicket(identity, new AuthenticationProperties());
+            context.Validated(ticket);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using Phobos.Api.App_Start;
+using Phobos.Api.Infrastructure;
+using System;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(Phobos.Api.Startup))]
 
@@ -10,6 +15,22 @@ namespace Phobos.Api
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            HttpConfiguration config = new HttpConfiguration();
+
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new SimpleOAuthAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
         }
     }
 }
