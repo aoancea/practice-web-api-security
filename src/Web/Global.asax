4a4065c107824f0e87514b31e04b880ed6952683
Alex Oancea <alx.oancea@gmail.com>
2016-11-06 23:08:45 +0200

    misc(web): remove Global.asax from Web project

--- src/Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Phobos.Web.MvcApplication" Language="C#" %>
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 
 [assembly: OwinStartup(typeof(Phobos.Web.Startup))]
 
@@ -9,12 +12,10 @@ namespace Phobos.Web
 	{
 		public void Configuration(IAppBuilder app)
 		{
-			ConfigureAuth(app);
-		}
-
-
-		public void ConfigureAuth(IAppBuilder app)
-		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
 	}
 }
