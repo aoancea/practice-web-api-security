503feb53683b8b02b26f0efadec8a8b354f2bf98
Alex Oancea <alx.oancea@gmail.com>
2016-11-09 00:02:42 +0200

    misc(web): integrated angular on login page

--- src/Web/app/app.module.js
+++ src/Web/app/app.module.js
@@ -1,5 +1,15 @@
 ﻿(function () {
 	'use strict';
 
-	angular.module('module.app', ['satellizer']);
+	angular.module('module.app', ['satellizer', 'toastr']);
+
+	angular
+		.module("module.app")
+		.config(config);
+
+	config.$injector = ['$authProvider'];
+
+	function config($authProvider) {
+		$authProvider.loginUrl = 'http://localhost:37227/token';
+	};
 })();
---
+++ src/Web/app/login/controllers/login.controller.js
@@ -0,0 +1,47 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('module.login')
+		.controller("LoginController", LoginController);
+
+	LoginController.$inject = ['$location', '$auth', 'toastr'];
+
+	function LoginController($location, $auth, toastr) {
+
+		var vm = this;
+		vm.user = {};
+		vm.login = login;
+		vm.authenticate = authenticate;
+
+		function login(user) {
+			$auth.login(user)
+			  .then(function () {
+			  	toastr.success('You have successfully signed in!');
+			  	$location.path('/');
+			  })
+			  .catch(function (error) {
+			  	toastr.error(error.data.message, error.status);
+			  });
+		};
+
+		function authenticate(provider) {
+			$auth.authenticate(provider)
+			  .then(function () {
+			  	toastr.success('You have successfully signed in with ' + provider + '!');
+			  	$location.path('/');
+			  })
+			  .catch(function (error) {
+			  	if (error.error) {
+			  		// Popup error - invalid redirect_uri, pressed cancel button, etc.
+			  		toastr.error(error.error);
+			  	} else if (error.data) {
+			  		// HTTP response error from server
+			  		toastr.error(error.data.message, error.status);
+			  	} else {
+			  		toastr.error(error);
+			  	}
+			  });
+		};
+	}
+})();
