2ec0f6b42d8ada48c45f58e90a0d1ecc4956c740
Alex Oancea <alx.oancea@gmail.com>
2016-11-08 22:29:41 +0200

    misc(web): added Login and Signup pages

---
+++ src/Web/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Phobos.Web.Controllers
+{
+	public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+		public ActionResult Signup()
+		{
+			return View();
+		}
+	}
+}
