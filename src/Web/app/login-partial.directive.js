dec2116f88131ad7224a788d2c8f34ad20c18080
Alex Oancea <alx.oancea@gmail.com>
2016-11-12 23:03:22 +0200

    misc(web): logout link

--- src/Web/app/login-partial.directive.js
+++ src/Web/app/login-partial.directive.js
@@ -5,16 +5,28 @@
         .module('module.app')
         .directive('loginPartial', loginPartial);
 
-	loginPartial.$inject = ['$window'];
+	loginPartial.$inject = [];
 
-	function loginPartial($window) {
+	LoginPartialController.$inject = ['$window'];
+
+	function loginPartial() {
 		var directive = {
-			replace: true,
-			restrict: 'EA',
-			scope: {
-			},
+			restrict: 'E',
+			controller: LoginPartialController,
+			controllerAs: 'vm',
 			templateUrl: 'Templates/LoginPartial',
 		};
 		return directive;
+	}
+
+	function LoginPartialController($window) {
+		var vm = this;
+		vm.logout = logout;
+
+		function logout() {
+			localStorage.removeItem('access_token');
+
+			$window.location.reload();
+		};
 	}
 })();
