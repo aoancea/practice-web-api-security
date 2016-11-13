9577604579ca1b2846a6b63bc9b7da0a92a2ae6b
Alex Oancea <alx.oancea@gmail.com>
2016-11-14 00:07:15 +0200

    misc(web): consolidate OAuth config registration

---
+++ src/Api/App_Start/OAuthConfig.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+namespace Phobos.Api.App_Start
+{
+	public class OAuthConfig
+	{
+		public static void Register(IAppBuilder app)
+		{
+			Infrastructure.Configuration.IConfig config = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Configuration.IConfig>();
+
+			app.UseExternalSignInCookie(Microsoft.AspNet.Identity.DefaultAuthenticationTypes.ExternalCookie);
+
+			OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions()
+			{
+				AllowInsecureHttp = true,
+				TokenEndpointPath = new PathString("/token"),
+				AccessTokenExpireTimeSpan = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Helpers.IAccessTokenHelper>().AccessTokenExpireTimeSpan(),
+				Provider = new Infrastructure.Providers.SimpleOAuthAuthorizationServerProvider(CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Helpers.IAccessTokenHelper>()),
+				RefreshTokenProvider = new Infrastructure.Providers.SimpleAuthenticationTokenProvider(CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Helpers.IAccessTokenHelper>()),
+				AccessTokenFormat = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Security.IAccessTokenSecureDataFormat>(),
+				RefreshTokenFormat = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Security.IRefreshTokenSecureDataFormat>()
+			};
+
+			OAuthBearerAuthenticationOptions oAuthBearerOptions = new OAuthBearerAuthenticationOptions()
+			{
+				AccessTokenFormat = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Security.IAccessTokenSecureDataFormat>()
+			};
+
+			app.UseOAuthAuthorizationServer(oAuthServerOptions);
+			app.UseOAuthBearerAuthentication(oAuthBearerOptions);
+
+			app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+			{
+				ClientId = config.GoogleClientId,
+				ClientSecret = config.GoogleClientSecret,
+				Provider = new Infrastructure.Providers.GoogleOAuth2AuthenticationProvider()
+			});
+
+			app.SetLoggerFactory(new Infrastructure.Logger.LoggerFactory());
+		}
+	}
+}
--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Owin;
-using Microsoft.Owin.Logging;
-using Microsoft.Owin.Security.Google;
-using Microsoft.Owin.Security.OAuth;
 using Owin;
 using Phobos.Api.App_Start;
-using Phobos.Api.Infrastructure.Configuration;
 
 [assembly: OwinStartup(typeof(Phobos.Api.Startup))]
 
@@ -18,44 +14,9 @@ namespace Phobos.Api
 
 			CompositionRoot.CompositionRoot.Register(CompositionRoot.CompositionRoot.Container, app);
 
-			ConfigureOAuth(app);
+			OAuthConfig.Register(app);
 
 			WebApiConfig.Register(app);
-		}
-
-		public void ConfigureOAuth(IAppBuilder app)
-		{
-			IConfig config = CompositionRoot.CompositionRoot.Container.GetInstance<IConfig>();
-
-			app.UseExternalSignInCookie(Microsoft.AspNet.Identity.DefaultAuthenticationTypes.ExternalCookie);
-
-			OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions()
-			{
-				AllowInsecureHttp = true,
-				TokenEndpointPath = new PathString("/token"),
-				AccessTokenExpireTimeSpan = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Helpers.IAccessTokenHelper>().AccessTokenExpireTimeSpan(),
-				Provider = new Infrastructure.Providers.SimpleOAuthAuthorizationServerProvider(CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Helpers.IAccessTokenHelper>()),
-				RefreshTokenProvider = new Infrastructure.Providers.SimpleAuthenticationTokenProvider(CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Helpers.IAccessTokenHelper>()),
-				AccessTokenFormat = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Security.IAccessTokenSecureDataFormat>(),
-				RefreshTokenFormat = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Security.IRefreshTokenSecureDataFormat>()
-			};
-
-			OAuthBearerAuthenticationOptions oAuthBearerOptions = new OAuthBearerAuthenticationOptions()
-			{
-				AccessTokenFormat = CompositionRoot.CompositionRoot.Container.GetInstance<Infrastructure.Security.IAccessTokenSecureDataFormat>()
-			};
-
-			app.UseOAuthAuthorizationServer(oAuthServerOptions);
-			app.UseOAuthBearerAuthentication(oAuthBearerOptions);
-
-			app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-			{
-				ClientId = config.GoogleClientId,
-				ClientSecret = config.GoogleClientSecret,
-				Provider = new Infrastructure.Providers.GoogleOAuth2AuthenticationProvider()
-			});
-
-			app.SetLoggerFactory(new Infrastructure.Logger.LoggerFactory());
 		}
 	}
 }
