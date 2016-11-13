d9ea5d1ebdb844c702b854c4a0bed33955a4f835
Alex Oancea <alx.oancea@gmail.com>
2016-11-13 23:54:42 +0200

    feat(security): register/signup page

---
+++ src/Web/app/signup/signup.controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('module.signup')
+        .controller('SignupController', SignupController);
+
+    SignupController.$inject = ['$location', '$http', 'toastr', '$window'];
+
+    function SignupController($location, $http, toastr, $window) {
+    	var vm = this;
+    	vm.user = {};
+    	vm.signup = signup;
+
+    	function signup(user) {
+    		var testUser = user;
+
+    		$http.post("http://localhost:37227/api/v1/account/register", user)
+				.then(function (response) {
+					$window.location.href = '/Login/Login';
+				})
+				.catch(function (error) {
+					toastr.error(error.data.message, error.status);
+				});
+    	}
+    }
+})();
