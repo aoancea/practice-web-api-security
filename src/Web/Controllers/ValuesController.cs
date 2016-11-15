13a1d76f8f41f7a4e6248be9b2795ac677c36dfb
Alex Oancea <alx.oancea@gmail.com>
2016-11-15 23:04:41 +0200

    misc(web): build a secure web page to display values from ValuesController #1

---
+++ src/Web/Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Phobos.Web.Controllers
+{
+	public class ValuesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Web/app/values/values.controller.js
@@ -0,0 +1,23 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('module.app')
+		.controller("ValuesController", ValuesController);
+
+	ValuesController.$inject = ['httpService'];
+
+	function ValuesController(httpService) {
+
+		var vm = this;
+		vm.values = [];
+
+		activate();
+
+		function activate() {
+			httpService.get("http://localhost:37227/api/v1/values").then(function (values) {
+				vm.values = values;
+			});
+		}
+	}
+})();
