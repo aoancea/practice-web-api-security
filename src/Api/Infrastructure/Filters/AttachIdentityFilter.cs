0cba8b37e6c585451fbf0cdfe12997e6458a2e1b
Alex Oancea <alx.oancea@gmail.com>
2016-11-20 18:02:31 +0200

    feat(api): Abstract away IPrincipal #3 - added Identity model

---
+++ src/Api/Infrastructure/Filters/AttachIdentityFilter.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Phobos.Api.Infrastructure.Filters
+{
+	public class AttachIdentityFilter : IActionFilter
+	{
+		public Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+		{
+			System.Security.Claims.ClaimsPrincipal principal = (System.Security.Claims.ClaimsPrincipal)actionContext.Request.GetRequestContext().Principal;
+
+			Identity.Identity Identity = JsonConvert.DeserializeObject<Identity.Identity>(principal.Claims.Single(x => x.Type == Infrastructure.Identity.IdentityConstants.IdentityClaimKey).Value);
+
+			actionContext.Request.Properties.Add(Infrastructure.Identity.IdentityConstants.IdentityRequestKey, Identity);
+
+			return continuation();
+		}
+
+		public bool AllowMultiple
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ src/Api/Infrastructure/Identity/Identity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Phobos.Api.Infrastructure.Identity
+{
+	public class Identity
+	{
+		public int ID { get; set; }
+
+		public Guid UID { get; set; }
+
+		public string Name { get; set; }
+	}
+
+	public static class IdentityConstants
+	{
+		public const string IdentityClaimKey = "Phobos_Api_Claim_Identity";
+
+		public const string IdentityRequestKey = "Phobos_Api_Request_Identity";
+	}
+}
