20e03acca57cb12241a55e9748a54c3da84a359a
Alex Oancea <alx.oancea@gmail.com>
2016-11-21 23:54:57 +0200

    feat(api): Abstract away IPrincipal #3 - added IdentityProvider

--- src/Api/Controllers/ValuesController.cs
+++ src/Api/Controllers/ValuesController.cs
@@ -6,9 +6,18 @@ namespace Phobos.Api.Controllers
 	[Authorize]
 	public class ValuesController : ApiController
 	{
+		private readonly Infrastructure.Identity.IIdentityProvider identityProvider;
+
+		public ValuesController(Infrastructure.Identity.IIdentityProvider identityProvider)
+		{
+			this.identityProvider = identityProvider;
+		}
+
 		[ActionName("Get")]
 		public async Task<string[]> GetAsync()
 		{
+			Infrastructure.Identity.Identity identity = identityProvider.Identity(this);
+
 			return await Task.FromResult(new string[5] { "string1", "string2", "string3", "string4", "string5" });
 		}
 	}
---
+++ src/Api/Infrastructure/Identity/IdentityProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace Phobos.Api.Infrastructure.Identity
+{
+	public interface IIdentityProvider
+	{
+		Identity Identity(ApiController controller);
+	}
+
+	public class IdentityProvider : IIdentityProvider
+	{
+		public Identity Identity(ApiController controller)
+		{
+			return controller.Request.Properties[IdentityConstants.IdentityRequestKey] as Identity;
+		}
+	}
+}
